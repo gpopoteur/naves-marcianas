2493242f8133e8e1687e15acf2b8f159f3e783dc
delacrmi <delacrmi@gmail.com>
2014-02-18 19:28:49 +0100

    Botones

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -1,6 +1,7 @@
 class Revision < ActiveRecord::Base
 
 	validates_presence_of :revisior
+	validates_presence_of :aeronave
 
 	has_many :revision_pasajeros
 	has_many :pasajeros, through: :revision_pasajeros
--- config.ru
+++ config.ru
@@ -2,3 +2,6 @@
 
 require ::File.expand_path('../config/environment',  __FILE__)
 run Rails.application
+
+#config.autoload_paths += %W(#{config.root}/extras)
+#config.active_record.schema_format = :ruby
